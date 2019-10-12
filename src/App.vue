ec8c035cdcdca91e8f25612fa8e0fe0eb168e949
神谷友樹 <jetree900@gmail.com>
2019-10-12 23:18:50 +0900

    cssをApp.vueに移動

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,12 @@
 </script>
 
 <style>
+  body{
+    margin: 0;
+    text-align: center;
+    background-color: #defcfc;
+    font-family: "YuGothicM","Hiragino Kaku Gothic ProN","メイリオ", 'arial-black';
+  }
   main{
     min-height: 80vh;
     max-width: 1200px;
